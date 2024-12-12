cd8437471d4d1368b605ced8059bdb2c7f442d6b
leont <leontievramalho@gmail.com>
2024-12-11 23:52:25 -0300

    tela-cadastro-e-minha-conta

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './components/home';
 import ProductPage from './components/PaginaProdutos';
 import PaginaBusca from './components/PaginaBusca'; 
+import Cadastro from './components/Cadastro';
+import MinhaConta from './components/MinhaConta';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} /> {}
+        <Route path="/cadastro" element={<Cadastro />} /> {}
+        <Route path="/minhaConta/:id" element={<MinhaConta/>} /> {}
         <Route path="/product/:id" element={<ProductPage />} /> {}
         <Route path="/search" element={<PaginaBusca />} /> {}
       </Routes>
